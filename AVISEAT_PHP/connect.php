4e0e34bb75c4123ea112016d55e16f4fabf970ef
Twinkling-star-space <XaverianBarahi111@gmail.com>
2025-02-15 16:03:07 +0545

    commit

---
+++ AVISEAT_PHP/connect.php
@@ -0,0 +1,32 @@
+<?php
+// Database connection parameters
+include 'connect.php';                           // Default password for MySQL
+$dbname = "seat_number"; // The name of the database you want to create or use
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Check if database exists
+$result = $conn->query("SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '$dbname'");
+
+if ($result->num_rows == 0) {
+    // Create database if it does not exist
+    $sql = "CREATE DATABASE $dbname";
+    if ($conn->query($sql) === TRUE) {
+        echo "Your Seat database recently createad";
+    } else {
+        echo "Error creating database: " . $conn->error;
+    }
+} else {
+    echo "Your Seat database already exists";
+}
+// Select the database
+$conn->select_db($dbname);
+
+$conn->close();
+?>
