5dcde9582c60573e23e5947c32381ab837e0e2b7
Diplal Thakur Barhi <XaverianBarahi111@gmail.com>
2025-09-21 16:28:22 +0545

    Update connect.php

--- AVISEAT_PHP/connect.php
+++ AVISEAT_PHP/connect.php
@@ -1,15 +1,16 @@
 <?php
-$servername="localhost";               // Database connection parameter
-$username="root"; // The name of the database you want to create or use
-$password ="";
-// Create connection
-$conn = new mysqli($servername, $username, $password);
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'diplal';
 
+// Create a connection
+$conn = new mysqli($host, $username, $password, $dbname);
+
+// Check the connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-else{
-    echo "Your server Connected successfully";
-}
-$conn->close();
+
+
 ?>
